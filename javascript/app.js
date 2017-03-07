a76f78fe5592fba1dbd63687843c909e8d8c5ad2
Kiên Cocktails <Kin Cocktails>
2017-03-08 01:12:32 +0700

    update

--- javascript/app.js
+++ javascript/app.js
@@ -8,6 +8,11 @@ var btn_record = document.getElementById("record");
 var btn_download = document.getElementById("download");
 var chunks;
 var mediaRecorder;
+var camera_stream;
+var desktop_stream;
+
+var checkbox_camera = document.getElementById("check_camera");
+var checkbox_audio = document.getElementById("check_audio");
 
 
 //
@@ -86,14 +91,14 @@ function onAccessApproved(desktop_id) {
     }, gotStream, errors);
 
     function gotStream(stream) {
-        console.log('getUserMedia() got stream: ', stream);
+       // console.log('getUserMedia() got stream: ', stream);
         window.stream = stream;
+        desktop_stream = stream;
         if (window.URL) {
-            video_local.src = window.URL.createObjectURL(stream);
+            video_local.src = window.URL.createObjectURL(desktop_stream);
         } else {
             video_local.src = stream;
         }
-       // video_local.src = URL.createObjectURL(stream);
 
         stream.onended = function () {
             if (desktop_sharing) {
@@ -103,76 +108,19 @@ function onAccessApproved(desktop_id) {
     }
 }
 
-// document.getElementById("btn_connect").addEventListener('click', function (e) {
-//     getCamera();
-// });
-
-function getCamera() {
-
-    // avigator.getUserMedia  = navigator.getUserMedia ||
-    //                       navigator.webkitGetUserMedia ||
-    //                       navigator.mozGetUserMedia ||
-    //                       navigator.msGetUserMedia;
-
-    // var video = document.querySelector('video');
-
-    // if (navigator.getUserMedia) {
-    //   navigator.getUserMedia({audio: true, video: true}, function(stream) {
-    //     document.getElementById('video_camera').src = window.URL.createObjectURL(stream);
-    //   }, function(e){
-
-    //     console.log(e);
-    //   });
-    var hdConstraints = {
-        video: {
-            mandatory: {
-                minWidth: 1280,
-                minHeight: 720
-            }
-        }
-    };
-
-    navigator.getUserMedia(hdConstraints, function (stream) {
-        document.getElementById('video_camera').src = window.URL.createObjectURL(stream);
-    }, errors);
-}
 
 var errors = function getUserMediaError(e) {
     console.log('getUserMediaError: ' + e);
 };
 
 
-//Record
-
-
-
-
-
-
 function togleRecord() {
 
 
-  //  var options = {mimeType: 'video/webm; codecs=vp9'};
     if (btn_record.textContent === "Start Recording") {
         startRecord();
-        //     //mediaRecorder = new MediaStreamRecorder(stream,options);
-        // mediaRecorder = new MediaRecorder(stream, options);
-        // console.log(stream);
-        //    // mediaRecorder.mimeType = 'video/webm';
-        //     mediaRecorder.ondataavailable = function (blob) {
-        //         chunks.push(blob);
-        //     };
-        //     mediaRecorder.start(10);
-        //     btn_record.textContent = "Stop Recording";
     }else if(btn_record.textContent === "Stop Recording"){
         stopRecording();
-        // mediaRecorder.stop();
-        // allBlod = new Blob(chunks,{type: 'video/webm;'});
-        // var videoURL = window.URL.createObjectURL(allBlod);
-        // video_local.src = videoURL;
-        // btn_play.disabled = false;
-        // btn_download.disabled = false;
-        // getScreen.disabled = false;
     }
 }
 
@@ -200,14 +148,33 @@ function startRecord() {
         return;
     }
 
+    console.log("camera",checkbox_camera.checked);
+    console.log("audio",checkbox_audio.checked);
+    if(checkbox_camera.checked == true ){
+        var options = { audio: checkbox_audio.checked, video: { width: 1280, height: 720 }};
+        //getCamera(options);
+        getCam(options);
+
+    }else if(checkbox_audio.checked == true){
+        console.log("-----------------------------\n");
+        navigator.getUserMedia({audio: true}, function(audioStream) {
+            // merge audio tracks into the screen
+            window.stream.addTrack(audioStream.getAudioTracks()[0]);
+        },errors);
+    }
 
 
     console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
     btn_record.textContent = 'Stop Recording';
     mediaRecorder.onstop = handleStop;
+    btn_play.disabled = true;
+    btn_download.disabled = true;
+    checkbox_audio.disabled = true;
+    checkbox_camera.disabled = true;
     mediaRecorder.ondataavailable = handleDataAvailable;
     mediaRecorder.start(10); // collect 10ms of data
     console.log('MediaRecorder started', mediaRecorder);
+    video_local.src = window.URL.createObjectURL(window.stream);
 }
 
 function handleDataAvailable(event) {
@@ -220,6 +187,8 @@ function handleStop(event) {
     btn_play.disabled = false;
     btn_download.disabled = false;
     getScreen.disabled = false;
+    checkbox_audio.disabled = false;
+    checkbox_camera.disabled = false;
 }
 function stopRecording() {
     mediaRecorder.stop();
@@ -227,6 +196,8 @@ function stopRecording() {
     console.log('Recorded Blobs: ', chunks);
     video_local.controls = true;
     btn_record.textContent = "Start Recording";
+    if(camera_stream != null)
+     camera_stream.stop();
 }
 function download() {
     console.log("download");
@@ -246,9 +217,6 @@ function download() {
 }
 
 function play() {
-    // allBlod = new Blob(chunks,{type: 'video/webm;'});
-    // var videoURL = window.URL.createObjectURL(allBlod);
-    // video_local.src = videoURL;
     var superBuffer = new Blob(chunks, {type: 'video/webm'});
     video_local.src = window.URL.createObjectURL(superBuffer);
     video_local.play();
@@ -258,17 +226,15 @@ function play() {
 //=============================================================================================
 var getCamera  = document.getElementById("getCamera");
 var video_camera = document.getElementById("video_camera");
-//getCamera.onclick = getCam();
- getCamera.addEventListener('click', function (e) {
-    getCam();
-});
-function getCam() {
-    navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: true
-    })
-        .then(function (stream) {
 
+ getCamera.addEventListener('click', function (e) {
+     var options = { audio: true, video: { width: 1280, height: 720 }};
+    getCam(options);
+});
+function getCam(options) {
+    navigator.mediaDevices.getUserMedia(options)
+        .then(function (stream) {
+            camera_stream = stream;
             video_camera.src = URL.createObjectURL(stream);
             //video_camera.play();
         });
